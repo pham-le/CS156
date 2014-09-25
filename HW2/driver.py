8727fbe19165b576d31b9883710e8e1695cc164e
chorro73 <chorro73@gmail.com>
2014-09-25 14:26:14 -0700

    Working on driver

---
+++ HW2/driver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+"""
+CS 156 Intro to AI - 01
+Homework 2, 10/01/2014
+
+Andres Chorro - 007340983
+Jannette Pham-Le - 007855120
+Justin Tieu - 007789678
+"""
+
+print "Would you like to go first (1) or second (2)?"
+turn = int(raw_input())
+if turn == 1:
+    print "You are first"
+else:
+    print "You are second"
