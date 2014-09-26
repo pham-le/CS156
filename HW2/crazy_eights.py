564fc97c6e98512a5e834dbe91c18ddfd8fcf875
Justin Tieu <tieujustin@gmail.com>
2014-09-26 12:25:42 -0700

    Updated functions for crazy eights

--- HW2/crazy_eights.py
+++ HW2/crazy_eights.py
@@ -12,24 +12,29 @@ Justin Tieu - 007789678
 import random
 
 class CrazyEights:
-	def __init__(self):
+	def __init__(self, currentPlayer):
 		"""A class that contains AI for crazy eights play"""
 		self.deck = self.initializeDeck()				#builds and shuffles deck
-		self.player_one_hand = self.initializeHand()	#draws 8 cards for player 1
-		self.player_two_hand = self.initializeHand()	#draws 8 cards for player 2
-		self.history = [] 								#new game contains no quadruple in its History
+		self.currentPlayer = currentPlayer
+		self.player_one_hand = self.draw(8)				#draws 8 cards for player 1
+		self.player_two_hand = self.draw(8)				#draws 8 cards for player 2
+		self.history = self.initializeHistory() 		#new game contains no quadruple in its History
 
 	def initializeDeck(self):
 		""" without repeats in the range between 0 - 51 representing the cards that are face down that have not been picked up by either player """
 		deck = range(52)								#build the deck (cards in order)
 		random.shuffle(deck)							#shuffle the deck
 		return deck
+		
+	def initializeHistory(self):
+		""" Helper function for constructor to make the first "non-move" and start the face-up pile"""
+		card = self.deck.pop()
+		return [(self.currentPlayer, card, card/13, 0)]
 
-	def initializeHand(self):
-		""" Helper function for constructor to grab initial hands from a deck without repeats in the range between 0 - 51"""
-		hand = self.deck[-8:]							#draw the last 8 cards
-		self.deck = self.deck[:-8]						#take them out of the deck
-		return hand
+	def draw(self, numCards):
+		cards = self.deck[-1 * numCards:]
+		self.deck = self.deck[:-1 * numCards]
+		return cards
 
 	def appendHistory(self, move):
 		""" 
@@ -38,21 +43,25 @@ class CrazyEights:
 		"""
 		self.history.append(move)
 
-	def validMove(card, move):
+	def validMove(self, card):
 	    """returns true if card (the card being played) is legally
 	    playable after move"""
-	    face_up = move[1]
+	    face_up = self.history[-1][1] #the face up card on to play on
 	    if((card - 7) % 13 == 0): #card is an 8, always playable
 	        return True
-
 	    elif(card / 13 == face_up / 13): #cards are the same suit
 	        return True
-
-	    elif(card % 13 == card % 13): #cards are the same value
+	    elif(card % 13 == face_up % 13): #cards are the same value
 	        return True
-
 	    else:
 	        return False
+
+	def executeMove(self, move):
+		self.appendHistory(move)
+		if(self.currentPlayer == 0):
+			self.player_one_hand.remove(move[1])
+		else: 
+			self.player_two_hand.remove(move[1])
 
 	def move(self, partial_state):
 		""" 
