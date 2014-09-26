ea872ad0e9f4e434aab03b582d175b3ab84a8e6a
Justin Tieu <tieujustin@gmail.com>
2014-09-25 20:31:42 -0700

    merged refactored files

--- HW2/crazy_eights.py
+++ HW2/crazy_eights.py
@@ -9,16 +9,63 @@ Jannette Pham-Le - 007855120
 Justin Tieu - 007789678
 """
 
+import random
+
 class CrazyEights:
-    """A class that contains AI for crazy eights play"""
-    def move(partial_state):
-    def movePefectKnowedge(state):
-    def moveRandom(state):
+	def __init__(self):
+		"""A class that contains AI for crazy eights play"""
+		self.deck = self.initializeDeck()				#builds and shuffles deck
+		self.player_one_hand = self.initializeHand()	#draws 8 cards for player 1
+		self.player_two_hand = self.initializeHand()	#draws 8 cards for player 2
+		self.history = [] 								#new game contains no quadruple in its History
 
+	def initializeDeck(self):
+		""" without repeats in the range between 0 - 51 representing the cards that are face down that have not been picked up by either player """
+		deck = range(52)								#build the deck (cards in order)
+		random.shuffle(deck)							#shuffle the deck
+		return deck
 
-hand = [] #without repeats in the range between 0 - 51
-deck = [] #without repeats in the range between 0 - 51 representing the cards that are face down that have not been picked up by either player
+	def initializeHand(self):
+		""" Helper function for constructor to grab initial hands from a deck without repeats in the range between 0 - 51"""
+		hand = self.deck[-8:]							#draw the last 8 cards
+		self.deck = self.deck[:-8]						#take them out of the deck
+		return hand
 
-state = () #tuple.. (deck, hand, partial_state), hand represents the other player's hand
-partial_state = () #tuple.. (face_up_card, suit, hand, history), face_up_card = current card next move must be made on, history = [moves]
-move = () #(player_num, face_up_card, suit, number_of_cards), faceupcard = card after the move, suit = next suit that must be played, numofcards = num of cards player picked up if any
+	def appendHistory(self, move):
+		""" 
+			History appends a move that is a quadruple (player_num, face_up_card, suit, number_of_cards) 
+			faceupcard = card after the move, suit = next suit that must be played, numofcards = num of cards player picked up if any
+		"""
+		self.history.append(move)
+
+	def validMove(card, move):
+	    """returns true if card (the card being played) is legally
+	    playable after move"""
+	    face_up = move[1]
+	    if((card - 7) % 13 == 0): #card is an 8, always playable
+	        return True
+
+	    elif(card / 13 == face_up / 13): #cards are the same suit
+	        return True
+
+	    elif(card % 13 == card % 13): #cards are the same value
+	        return True
+
+	    else:
+	        return False
+
+	def move(self, partial_state):
+		""" 
+			Receives a partial_state that is a tuple (face_up_card, suit, hand, history)
+			face_up_card = current card next move must be made on, history = [moves]
+			Plays a card that is of the same suit or same number or an 8 
+		"""		
+		return partial_state
+
+	def movePefectKnowedge(self, state):
+		""" Receives a tuple (deck, hand, partial_state), hand represents the other player's hand """
+		return 
+
+	def isGameOver(self):
+		""" Checks if the game is over yet """
+		return not(len(self.deck) > 0)
--- HW2/driver.py
+++ HW2/driver.py
@@ -9,23 +9,9 @@ Jannette Pham-Le - 007855120
 Justin Tieu - 007789678
 """
 
-import random
+from crazy_eights import CrazyEights 
 
-def ValidMove(card, move):
-    """returns true if card (the card being played) is legally
-    playable after move"""
-    face_up = move[1]
-    if((card - 7) % 13 == 0): #card is an 8, always playable
-        return True
-
-    elif(card / 13 == face_up / 13): #cards are the same suit
-        return True
-
-    elif(card % 13 == card % 13): #cards are the same value
-        return True
-
-    else:
-        return False
+ce = CrazyEights()	#creates a Crazy Eights game
 
 def stringToMove(string):
     """Converts a string that represents a move (eg: '(0, 25, 3, 0)')
@@ -41,27 +27,29 @@ if turn == 0:
 else:
     print "You are second"
 
-deck = range(52)#build the deck (cards in order)
-random.shuffle(deck)#shuffle the deck
 
-player_hand = deck[-8:]#draw the last 8 cards
-deck = deck[:-8]#take them out of the deck
+deck = ce.deck
+player_hand = ce.player_one_hand
+computer_hand = ce.player_two_hand
 
-computer_hand = deck[-8:]#computer draws the next 8
-deck = deck[:-8]#take them out of the deck
-
-last_move = (1, deck.pop(), 0, 0)#represents the initial "non-move" that starts the game
-history = [last_move]
+last_move = (1, deck.pop(), 0, 0) #represents the initial "non-move" that starts the game
 
 print "Last move:", last_move
 print "Your hand:", player_hand
 
 while True:
     print "Enter your move (0, card you're playing, suit you're changing to, 0)"
-    move = stringToMove(raw_input())#input the human's move
-    if ValidMove(move[1], last_move):
-        history.append(last_move)
-        print history
+    move = stringToMove(raw_input()) #input the human's move
+    if ce.validMove(move[1], last_move):
+        ce.appendHistory(last_move)
+        print ce.history
         break
     else:
         print "Your move is invalid"
+
+# while(ce.isGameOver()) {
+# 	print "player hand:", player_hand
+# 	print "computer hand:", computer_hand
+# }
+# print ce.deck
+# print ce.move(player_hand[0]) 
