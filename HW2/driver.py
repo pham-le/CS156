8bb5345fd7328a3268418f387366ab56b9b41f16
Justin Tieu <tieujustin@gmail.com>
2014-09-26 12:26:27 -0700

    updates to stringToMove

--- HW2/driver.py
+++ HW2/driver.py
@@ -11,12 +11,11 @@ Justin Tieu - 007789678
 
 from crazy_eights import CrazyEights 
 
-ce = CrazyEights()	#creates a Crazy Eights game
 
 def stringToMove(string):
     """Converts a string that represents a move (eg: '(0, 25, 3, 0)')
     to an actual python tupple"""
-    string = string.strip("()").replace(",", "").split()
+    string = string.strip("()").replace(", ", " ").replace(",", " ").split()
     string = [int(s) for s in string]
     return tuple(string)
 
@@ -27,29 +26,23 @@ if turn == 0:
 else:
     print "You are second"
 
-
+#alias
+ce = CrazyEights(turn)	#creates a Crazy Eights game
 deck = ce.deck
 player_hand = ce.player_one_hand
 computer_hand = ce.player_two_hand
 
-last_move = (1, deck.pop(), 0, 0) #represents the initial "non-move" that starts the game
-
-print "Last move:", last_move
 print "Your hand:", player_hand
 
+
 while True:
+    print ce.history
     print "Enter your move (0, card you're playing, suit you're changing to, 0)"
     move = stringToMove(raw_input()) #input the human's move
-    if ce.validMove(move[1], last_move):
-        ce.appendHistory(last_move)
+    if ce.validMove(move[1]):
+        ce.executeMove(move)
         print ce.history
+        print "Your updated hand:", player_hand
         break
     else:
         print "Your move is invalid"
-
-# while(ce.isGameOver()) {
-# 	print "player hand:", player_hand
-# 	print "computer hand:", computer_hand
-# }
-# print ce.deck
-# print ce.move(player_hand[0]) 
