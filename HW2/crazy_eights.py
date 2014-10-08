2a1aec45e24e4465cff4e5bdbba90e4fa6d26865
Justin Tieu <tieujustin@gmail.com>
2014-10-08 03:40:21 -0700

    moved into single hw2 directory

--- HW2/crazy_eights.py
+++ HW2/crazy_eights.py
@@ -2,7 +2,7 @@
 
 """
 CS 156 Intro to AI - 01
-Homework 2, 10/01/2014
+Homework 2, 10/06/2014
 
 Andres Chorro - 007340983
 Jannette Pham-Le - 007855120
@@ -10,148 +10,219 @@ Justin Tieu - 007789678
 """
 
 import random
-
+import copy
 
 class CrazyEights:
-    def __init__(self, currentPlayer):
-        """A class that contains AI for crazy eights play"""
-        self.deck = self.initializeDeck()  # builds and shuffles deck
-        self.currentPlayer = currentPlayer
-        self.human_hand = []
-        self.computer_hand = []
-        self.draw(0, 8)# draws 8 cards for player 1
-        self.draw(1, 8)  # draws 8 cards for player 2
-        self.history = self.initializeHistory()  # new game contains no quadruple in its History
+    """Contains fuctions that can be used to run a game of Crazy Eights"""
 
-    def initializeDeck(self):
-        """ without repeats in the range between 0 - 51 representing the cards that are face down that have not been picked up by either player """
-        deck = range(52)  # build the deck (cards in order)
-        random.shuffle(deck)  # shuffle the deck
-        return deck
+    def get_suit(self, card):
+        """returns a suit value for the card: (spades: 0, hearts: 1, diamonds: 2, clubs: 3)"""
+        return card / 13
 
-    def initializeHistory(self):
-        """ Helper function for constructor to make the first "non-move" and start the face-up pile"""
-        card = self.deck.pop()
-        return [(self.currentPlayer, card, card / 13, 0)]
+    def get_value(self, card):
+        """returns the numeric value of a card"""
+        return card % 13
 
-    def draw(self, playernum, numCards):
-    	""" Helper function to draw numcards number of cards from the deck
-        for player payernum (0 for human, 1 for computer)"""
-        if numCards > len(self.deck):
-            numCards = len(self.deck)
-        cards = self.deck[-1 * numCards:]
-        self.deck = self.deck[:-1 * numCards]
-        if playernum == 0:
-            self.human_hand = self.human_hand + cards
+    def get_initial_state(self, starting_player):
+        """returns a tuple representing a complete game state right after hands have been
+        delt, and there is 1 card face-up."""
+        deck = range(52)
+        random.shuffle(deck)
+
+        # draw each hand from the deck
+        human_hand = deck[-8:]
+        deck = deck[:-8]
+        computer_hand = deck[-8:]
+        deck = deck[:-8]
+
+        face_up_card = deck.pop()
+        suit = self.get_suit(face_up_card)
+
+        if starting_player is 0:
+            history = [(1, face_up_card, suit, 0)]
+            partial_state = (face_up_card, suit, human_hand, history)
+            initial_state = (deck, computer_hand, partial_state)
         else:
-            self.computer_hand = self.computer_hand + cards
+            history = [(0, face_up_card, suit, 0)]
+            partial_state = (face_up_card, suit, computer_hand, history)
+            initial_state = (deck, human_hand, partial_state)
+        return initial_state
 
-    def validMove(self, move):
-        """returns true if card (the card being played) is legally
-        playable after move"""
-        card = move[1]
-        face_up = self.history[-1][1]  # the face up card to play on
-        suit = face_up/13 #the suit to play on
-        if (move[3] == 0 and #trying to play
-                ((self.currentPlayer == 0 and card not in self.human_hand) or
-                (self.currentPlayer == 1 and card not in self.computer_hand))):
-            print "INVALID: Card is not in your hand!"
-            return False
-        if face_up in [7, 20, 33, 46]:  #8 face-up, reset suit
-            suit = self.history[-1][2]
-        opponent_drew = self.history[-1][3] != 0  #whether the opponent drew last turn
-        if ((len(self.history) > 1) #not first turn
-                and (face_up in [1, 14, 27, 40]) #face-up 2
-                and (not opponent_drew) #opponent didn't draw
-                and (card not in [1, 14, 27, 40]) #you're not playing a 2
-                and (move[3] == 0)): #you're trying to not draw
-            print "INVALID: You must play a 2 or draw"
-            return False
-        elif (card in [7, 20, 33, 46]): #8 always valid at this point
-            return True 
-        elif ((card / 13 !=  suit) 
-                and (card % 13 != face_up % 13)):  #cards don't match suit or value
-            print "INVALID: Either suit or value must match the face-up card"
-            return False
-        else:
-            return True
-
-    def executeSpecialMove(self, card):
-        """ Executes special cards."""
-        if (card == 11):  # card is the Queen of Spades, make opponent draw 5
-            if self.currentPlayer == 0:  # if player is human
-                self.history.append((1, card, 0, 5))
-                self.draw(1, 5)
-            else:  # if player is ai
-                self.history.append((0, card, 0, 5))
-                self.draw(0, 5)
-        # elif card in [1, 14, 27, 40]: #card is 2
-        #     if self.currentPlayer == 0:  # if player is human
-        #         self.history.append((1, card, card/13, 2))
-        #         self.draw(1, 2)
-        #     else:  # if player is ai
-        #         self.history.append((0, card, card/13, 2))
-        #         self.draw(0, 2)
-        elif card in [10, 23, 36, 49]:  # card is Jack, make opponent skip turn
-            if self.currentPlayer == 0:
-                self.history.append((1, card, card/13, 0))
-            else:
-                self.history.append((0, card, card/13, 0))
-        else:
-            if self.currentPlayer == 0:
-                self.currentPlayer = 1
-            else:
-                self.currentPlayer = 0
-
-    def executeMove(self, move):
-        """ Assumes player's move is valid and continues to execute the move """
-        self.history.append(move)
-        if self.currentPlayer == 0:
-            if(move[3] == 0): #no draw; card was played
-                self.human_hand.remove(move[1])
-                self.executeSpecialMove(move[1])
-            else:
-                self.draw(0, move[3])
-                self.currentPlayer = 1
-        else:
-            if(move[3] == 0): #no draw; card was played
-                self.computer_hand.remove(move[1])
-                self.executeSpecialMove(move[1])
-            else:
-                self.draw(1, move[3])
-                self.currentPlayer = 0
-
-    def move(self, partial_state):
-        """
-            Receives a partial_state that is a tuple (face_up_card, suit, hand, history)
-            face_up_card = current card next move must be made on, history = [moves]
-            Plays a card that is of the same suit or same number or an 8
-        """
-        hand = partial_state[2]
+    def actions(self, original_partial_state):
+        """returns the set of moves that are playable in the state."""
+        partial_state = copy.deepcopy(original_partial_state)
+        face_up_card, face_up_suit, hand, history = partial_state
+        face_up_value = self.get_value(face_up_card)
+        player = 0 if (history[-1][0] is 1) else 1 #the player who must play next 
+        actions = []
+        if len(history) > 1:  # special cards only apply past turn 1
+            if history[-1][3] is 0:  # opponent didn't draw, so their special cards apply
+                if face_up_card is 11:  #queen of spades, can only draw 5
+                    actions.append((player, face_up_card, face_up_suit, 5))
+                    return actions
+                if face_up_value is 1:  #2 is face-up
+                    for card in hand:
+                        if self.get_value(card) is face_up_value:  #you have a 2
+                            actions.append((player, card, self.get_suit(card), 0))
+                    two_counter = 1  #keep track of how much you must draw
+                    while (two_counter < 5
+                           and len(history) > (two_counter + 1)  #don't want to check first faceup card
+                           and self.get_value(history[-(two_counter + 1)][1]) is 1
+                           and history[-(two_counter + 1)][3] is 0):  #previous card is 2
+                        two_counter = two_counter + 1
+                    actions.append((player, face_up_card, face_up_suit, 2 * two_counter))
+                    return actions
+                if (face_up_value is 10) and (
+                            history[-2][1] != face_up_card):  #jack face up, and opponent wasn't skipped
+                    actions.append((player, face_up_card, face_up_suit, 0))
+                    return actions #you must do nothing
         for card in hand:
-            move = (1, card, card / 13, 0)
-            if self.validMove(move):
-                return move
-        return (1, self.history[-1][1], self.history[-1][2], 1)
+            if self.get_value(card) is 7:
+                for i in range(4):
+                    actions.append((player, card, i, 0))
+            elif (self.get_suit(card) is face_up_suit
+                  or self.get_value(card) is face_up_value):
+                actions.append((player, card, self.get_suit(card), 0))
+        actions.append((player, face_up_card, face_up_suit, 1))
+        return actions
 
-    def movePefectKnowedge(self, state):
-        """ Receives a tuple (deck, hand, partial_state), hand represents the other player's hand """
-        return
+    def result(self, original_state, original_move):
+        """returns the resulting state after move has been applied to it"""
+        state = copy.deepcopy(original_state)
+        move = copy.deepcopy(original_move)
+        deck, other_hand, partial_state = state
+        face_up_card, face_up_suit, our_hand, history = partial_state
+        player_moved, played_card, played_suit, drawn_cards = move
 
-    def isGameOver(self):
-        """ Checks if the game is over yet """
-        return [] in [self.deck, self.human_hand, self.computer_hand]
+        if drawn_cards is not 0:  # player drew cards
+            if drawn_cards > len(deck):  # player drew more cards than in deck
+                drawn_cards = len(deck)
+            our_hand = our_hand + deck[-1 * drawn_cards:]
+            deck = deck[:-1 * drawn_cards]
+        elif played_card is not face_up_card: #player was not skipped
+            our_hand.remove(played_card)
+        history.append(move)
+        partial_state = (played_card, played_suit, other_hand, history)
+        state = (deck, our_hand, partial_state)
+        return state
 
-    def getWinner(self):
-        """ Assumes game is over. Gets winner of game based on hands """
-        if(len(self.human_hand) < len(self.computer_hand)):
-            return "Player wins!"
-        elif(len(self.human_hand) > len(self.computer_hand)):
-            return "Computer wins!"
+
+    def game_over(self, state):
+        """returns true if the game is over in this state"""
+        # check if deck, opponent hand, or your hand is empty
+        deck, opponent_hand, partial_state = state
+        your_hand = partial_state[2]
+        return [] in [deck, opponent_hand, your_hand]
+
+    def utility(self, state, player):
+        """Returns the utility (0 for a loss, 1 for a win) of a final state for a designated player:
+        1 for computer, 0 for human"""
+        
+        last_player = state[2][3][-1][0]
+        current_player = 0 if (last_player is 1) else 1
+        opponent_hand = state[1]
+        current_player_hand = state[2][2]
+        winner = -1
+        if len(opponent_hand) > len(current_player_hand):
+            winner = current_player
+        elif len(opponent_hand) < len(current_player_hand):
+            winner = last_player
+        elif min(opponent_hand) < min(current_player_hand):
+            winner = last_player
         else:
-            if min(self.human_hand) < min(self.computer_hand):
-                return "Player wins!"
-            elif min(self.computer_hand) < min(self.human_hand):
-                return "Computer wins!"
-            else:
-                return "error"
+            winner = current_player
+        if player is winner:
+            return 1
+        else:
+            return 0
+
+    def evaluate(self, state):
+        opponent_hand = state[1]
+        your_hand = state[2][2]
+        return len(opponent_hand) - len(your_hand)
+
+    # def move(self, partial_state):
+    #     deck = range(52) 
+    #     deck = [card for card in deck if card not in (partial_state[2] + partial_state[3])]
+    #     list_of_moves = []
+    #     for x in range(0, 100):
+    #         random.shuffle(deck)
+    #         state = (deck, partial_state[2], partial_state)
+    #         list_of_moves.append(self.move_perfect_knowledge(state))
+        
+    #     numMax = 0
+    #     maxitem = None
+    #     for x in set(l):
+    #         count =  l.count(x)
+    #     if count > numMax:
+    #         numMax = count
+    #         maxitem = x
+    #     return maxitem
+
+    # def alpha(self, state):
+    #     """Picks a move based on the state and the minimax algorithm with
+    #     alpha-beta pruning and the limiting hueristic: hand - opponent hand"""
+    #     v = max_value(state, -10000, 10000)
+    #     actions = actions(state)
+    #     for a in actions:
+    #         if v in a:
+    #             return a
+        
+    # def max_value(self, state, alpha, beta):
+    #     if self.game_over(state):
+    #         return utility(state, 1)
+    #     v = float(-inf)
+    #     for a in actions(state):
+    #         v = max(v, min_value(result(state, a), alpha, beta))
+    #         if v >= beta:
+    #             return v
+    #         alpha = max(alpha, v)
+    #     return v
+
+    # def min_value(self, state, alpha, beta):
+    #     if self.game_over(state):
+    #         return utility(state, 1)
+    #     v = float(inf)
+    #     for a in actions(state):
+    #         v = min(v, max_value(result(state, a), alpha, beta))
+    #         if v <= alpha:
+    #             return v
+    #         beta = min(beta, v)
+    #     return v
+
+    # def dumb_move(self, state):
+    #     """pick the first option from the available moves"""
+    #     return self.actions(state[2])[0]
+
+
+    #these have no alpha-beta pruning.
+
+    def move(self, state):
+        return move_perfect_knowledge(state)
+
+    def move_perfect_knowledge(self, state):
+        d = 0
+        values = [] #list of (value, move) tuples
+        for move in self.actions(state[2]):
+            values.append((self.min_value(self.result(state, move), d), move))
+        return max(values)[1]
+
+    def min_value(self, state, d):
+        if self.game_over(state):
+            return self.utility(state, 1)
+        if d > 3:
+            return self.evaluate(state)
+        v = 100000
+        for move in self.actions(state[2]):
+            v = min(v, self.max_value(self.result(state, move), d + 1))
+        return v
+
+    def max_value(self, state, d):
+        if self.game_over(state):
+            return self.utility(state, 1)
+        if d > 3:
+            return self.evaluate(state)
+        v = 100000
+        for move in self.actions(state[2]):
+            v = max(v, self.min_value(self.result(state, move), d + 1))
+        return v
--- HW2/driver.py
+++ HW2/driver.py
@@ -2,14 +2,17 @@
 
 """
 CS 156 Intro to AI - 01
-Homework 2, 10/01/2014
+Homework 2, 10/06/2014
 
 Andres Chorro - 007340983
 Jannette Pham-Le - 007855120
 Justin Tieu - 007789678
 """
 
+import random
 from crazy_eights import CrazyEights
+
+game = CrazyEights()
 
 
 def stringToMove(string):
@@ -20,51 +23,54 @@ def stringToMove(string):
     return tuple(string)
 
 
+def printState(state):
+    deck, opponent_hand, partial_state = state
+    face_up_card, suit, your_hand, history = partial_state
+    print "\nCards in deck:", len(deck)
+    print "Opponent hand length:", len(opponent_hand)
+    print "History:", history
+    print "Face up:", face_up_card
+    print "Your hand:", your_hand
+    print "Your options:", game.actions(partial_state)
+
+
 print "Would you like to go first (1) or second (2)?"
-turn = int(raw_input()) - 1  # player is 0, computer is 1
-if turn == 0:
+current_player = int(raw_input()) - 1  # 0 represents human, 1 for computer
+human_turn = (current_player is 0)  # true when it's the human's turn
+
+current_state = game.get_initial_state(current_player)
+
+if human_turn:
     print "You are first"
 else:
     print "You are second"
 
-# creates a Crazy Eights game
-ce = CrazyEights(turn)
-
-while not ce.isGameOver():
-    print "\n\nGame History:", ce.history
-    while True and ce.currentPlayer == 0:
-        print "Cards left in deck:", len(ce.deck)
-        print "Cards left in computer hand:", len(ce.computer_hand)
-        print "Face up card:", ce.history[-1][1]
-        print "Your current hand:", ce.human_hand
-        print "Enter your move (0, card you're playing, suit you're changing to, 0):"
-
-
-        # input the human's move
+while not game.game_over(current_state):
+    if human_turn:
+        print "\nHUMAN TURN:"
+        printState(current_state)
+        actions = game.actions(current_state[2])
         move = stringToMove(raw_input())
+        while move not in actions:
+            print "Invalid Move"
+            move = stringToMove(raw_input())
+        current_state = game.result(current_state, move)
+        human_turn = not human_turn
+    else:
+        print "\nCOMPUTER TURN:"
+        printState(current_state)
+        # move = game.move(current_state[2])
+        move = game.move_perfect_knowledge(current_state)
+        print "COMPUTER MOVE:", move
+        current_state = game.result(current_state, move)
+        human_turn = not human_turn
 
-        if ce.validMove(move):
-            ce.executeMove(move)
-            print "Your updated hand:", ce.human_hand
-            break
-        else:
-            print "Your move is invalid"
+print game.utility(current_state, 0)
+if game.utility(current_state, 0) is 1:
+    print "\nHUMAN WINS!"
+else:
+    print "\nCOMPUTER WINS!"
+# create variables that will be used to create the initial stat
 
-    if ce.isGameOver(): #check for end after player's turn
-        break
 
-    if(ce.currentPlayer == 1):
-        print "\nComputer's turn"
-        # partial_state that is a tuple (face_up_card, suit, hand, history)
-        face_up_card = ce.history[-1][1]
-        suit = ce.history[-1][2]
-        partial_state = (face_up_card, suit, ce.computer_hand, ce.history)
-        computer_move = ce.move(partial_state)
 
-        # checks if given move is the last move in history. if it is, end game
-        if computer_move in ce.history:
-            break
-        else:
-            ce.executeMove(computer_move)
-        print("End of Computer's turn")
-print ce.getWinner()
