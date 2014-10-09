86ec04547aedcc5ecbe9fddecc250f169c07ce77
Justin Tieu <tieujustin@gmail.com>
2014-10-08 20:29:53 -0700

    initial commit for hw3

---
+++ HW3/csp_solver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#CS 156 Intro to AI - 01
+#Homework 3, 10/27/2014
+#Andres Chorro		- 007340983
+#Jannette Pham-Le	- 007855120
+#Justin Tieu		- 007789678
+
+import sys 
+
+if(len(sys.argv) == 3):
+    problem_filename = sys.argv[1]
+    use_forward_check_flag = sys.argv[2]
+else:
+	print "Invalid input. Please use the format:"
+	print "python csp_solver.py problem_filename use_forward_check_flag"
+
+
